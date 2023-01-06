fdf647ff496d9b13758008582a5023f3084a1552
morzhanov <vlad.morzhanov@gmail.com>
2023-01-06 23:13:53 +0200

    revert iport changes

--- server/auth/sso/sso.go
+++ server/auth/sso/sso.go
@@ -14,6 +14,7 @@ import (
 
 	pkgrand "github.com/argoproj/pkg/rand"
 	"github.com/coreos/go-oidc/v3/oidc"
+	"github.com/go-jose/go-jose/v3"
 	"github.com/go-jose/go-jose/v3/jwt"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/oauth2"
